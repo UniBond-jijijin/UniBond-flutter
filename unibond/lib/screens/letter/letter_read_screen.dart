7389d753012a490a6c2e52b27f990cece5b51dc5
Nyeong <iwbm312@gmail.com>
2023-10-30 12:27:55 +0900

    Feat: 편지 읽는 화면 추가

--- unibond/lib/screens/letter/letter_read_screen.dart
+++ unibond/lib/screens/letter/letter_read_screen.dart
@@ -1,10 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:unibond/models/letter.dart';
 
-class LetterReadScreen extends StatelessWidget {
-  const LetterReadScreen({super.key});
+class LetterReadScreen extends StatefulWidget {
+  final Letter letter;
+
+  const LetterReadScreen({super.key, required this.letter});
 
   @override
+  _LetterReadScreenState createState() => _LetterReadScreenState();
+}
+
+class _LetterReadScreenState extends State<LetterReadScreen> {
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("받은 편지"),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(
+              widget.letter.isBookmarked
+                  ? Icons.bookmark
+                  : Icons.bookmark_border,
+            ),
+            onPressed: () {
+              setState(() {
+                widget.letter.isBookmarked = !widget.letter.isBookmarked;
+              });
+              // 북마크 상태->db저장 필요
+            },
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                widget.letter.title,
+                style: const TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 16),
+              Text(
+                widget.letter.content,
+                style: const TextStyle(
+                  fontSize: 18,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
