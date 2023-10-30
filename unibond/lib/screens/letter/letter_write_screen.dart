26d35d580fc38f19799cbbae4172fe2a299ff082
Nyeong <iwbm312@gmail.com>
2023-10-30 12:28:07 +0900

    Feat: 편지 쓰기 화면 추가

--- unibond/lib/screens/letter/letter_write_screen.dart
+++ unibond/lib/screens/letter/letter_write_screen.dart
@@ -1,10 +1,69 @@
 import 'package:flutter/material.dart';
 
-class LetterWriteScreen extends StatelessWidget {
+class LetterWriteScreen extends StatefulWidget {
   const LetterWriteScreen({super.key});
 
   @override
+  _LetterWriteScreenState createState() => _LetterWriteScreenState();
+}
+
+class _LetterWriteScreenState extends State<LetterWriteScreen> {
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _contentController = TextEditingController();
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _contentController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("편지 작성"),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.send),
+            onPressed: () {
+              // 편지 전송하는 코드 추가
+            },
+          )
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _titleController,
+              decoration: const InputDecoration(
+                labelText: "제목",
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 16),
+            Expanded(
+              child: TextField(
+                controller: _contentController,
+                maxLines: null,
+                expands: true,
+                decoration: const InputDecoration(
+                  hintText: "내용을 입력하세요...",
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
