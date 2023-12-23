86a22d519e35dfec1cc2ef5c197fdd2bcc0d63ac
Nyeong <iwbm312@gmail.com>
2023-12-23 22:23:02 +0900

    #10 Feat: 회원가입 기능 일부 구현
    - 데이터 구조 수정 필요
    - login과 repository dto 부분 다름

--- unibond/lib/main.dart
+++ unibond/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:unibond/view/screens/home_screen.dart';
-import 'package:unibond/view/screens/user/login_screen.dart';
+import 'package:unibond/view/screens/user/join_screen.dart';
 
 void main() {
   runApp(const MaterialApp(
@@ -20,7 +19,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
         useMaterial3: true,
       ),
-      home: const HomeScreen(),
+      home: JoinScreen(),
       // home: DetailScreen(
       //   id: 0,
       // ), // 임시 스플래시화면
