4394b65e7bb17f4efdc649f0883ce5ef672d061a
Nyeong <iwbm312@gmail.com>
2023-12-23 22:21:38 +0900

    Fix: 테스트용 로그인 파트 null 오류 해결
    - 테스트용 서버로 개발하던 부분 나머지 구현

--- unibond/lib/controller/user_controller.dart
+++ unibond/lib/controller/user_controller.dart
@@ -1,12 +1,42 @@
 import 'package:get/get.dart';
+import 'package:unibond/controller/dto/join_req_dto.dart';
 import 'package:unibond/domain/user/user_repository.dart';
 import 'package:unibond/util/jwt.dart';
 
 class UserController extends GetxController {
   final UserRepository _userRepository = UserRepository();
 
-  Future<void> login(String username, String password) async {
+  // 일단 이 함수는 post 요청만 한다고 가정
+  Future<String> join(
+    String userId,
+    String password,
+    String diseaseId,
+    String diseaseTiming,
+    String gender,
+    String nickname,
+    String bio,
+    String interestList,
+  ) async {
+    JoinReqDto joinReqDto = JoinReqDto(userId, password, diseaseId,
+        diseaseTiming, gender, nickname, bio, interestList);
+
+    print(joinReqDto.userId);
+    print(joinReqDto.password);
+    print(joinReqDto.nickname);
+    var userIdNum = await _userRepository.join(joinReqDto);
+    print(userIdNum.toString());
+    return userIdNum;
+  }
+
+  // 테스트 서버용 로그인 메서드
+  Future<String> login(String username, String password) async {
     String token = await _userRepository.login(username, password);
-    jwtToken = token;
+
+    if (token != "-1") {
+      jwtToken = token;
+      print("jwtToken : $jwtToken");
+    }
+
+    return token;
   }
 }
--- unibond/lib/view/screens/user/login_screen.dart
+++ unibond/lib/view/screens/user/login_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:unibond/view/screens/home_screen.dart';
 import 'package:unibond/controller/user_controller.dart';
-import 'package:unibond/util/jwt.dart';
 import 'package:unibond/view/screens/user/join_screen.dart';
 import 'package:unibond/util/validator_util.dart';
 import 'package:unibond/view/widgets/custom_text_form_field.dart';
@@ -10,6 +9,10 @@ import 'package:unibond/view/widgets/custon_elevated_button.dart';
 
 class LoginScreen extends StatelessWidget {
   final _formKey = GlobalKey<FormState>();
+
+  final UserController u = Get.put(UserController());
+  final _nicknameController = TextEditingController();
+  final _passwordController = TextEditingController();
 
   LoginScreen({super.key});
 
@@ -49,23 +52,28 @@ class LoginScreen extends StatelessWidget {
           CustomTextFormField(
             hint: "아이디",
             funvalidator: validateId,
+            controller: _nicknameController,
           ),
           CustomTextFormField(
+            controller: _passwordController,
             hint: "비밀번호",
             funvalidator: validatePassword,
           ),
           const SizedBox(height: 20),
           CustomElevatedButton(
             text: "로그인",
-            screenRoute: () {
+            screenRoute: () async {
               if (isValid(_formKey)) {
                 // 임시
-                UserController u = UserController();
-                u.login("jinyshin", "1234");
-                print(jwtToken);
-                // UserRepository u = UserRepository();
-                // u.login("jinyshin", "1234");
-                Get.to(() => const HomeScreen());
+                var token = await u.login(_nicknameController.text.trim(),
+                    _passwordController.text.trim());
+                if (token != "-1") {
+                  print("토큰 정상적으로 받음");
+                  Get.to(() => const HomeScreen());
+                } else {
+                  print("토큰 못받음");
+                  Get.snackbar("로그인 시도", "로그인 실패");
+                }
               }
             },
           ),
