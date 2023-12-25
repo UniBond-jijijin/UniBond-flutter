b00e4d80da8a734a12eeb0cb9d8ca95c9786eb40
Nyeong <iwbm312@gmail.com>
2023-12-25 19:00:45 +0900

    Feat: 게시물 작성 api 연결 완료

--- unibond/lib/domain/post/post_provider.dart
+++ unibond/lib/domain/post/post_provider.dart
@@ -4,6 +4,15 @@ import 'package:unibond/util/userIdNum.dart';
 const host = "http://3.35.110.214";
 
 class PostProvider extends GetConnect {
-  Future<Response> getPostsList() => get("$host/api/v1/community/experience",
+  // TODO: question, experience 메서드 따로 만들기
+  Future<Response> getPostsList() => get("$host/api/v1/community/question",
       headers: {"Authorization": userIdNum ?? ""});
+  Future<Response> uploadPost(Map data) {
+    print(data);
+
+    return post(
+        "$host/api/v1/community/question",
+        headers: {"Authorization": "4"},
+        data);
+  }
 }
