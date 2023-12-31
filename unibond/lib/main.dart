57771411adb99aee69077397c689a104b1c7d19c
Nyeong <iwbm312@gmail.com>
2023-12-31 11:33:08 +0900

    Design: 나의 프로필 조회 화면 UI

--- unibond/lib/main.dart
+++ unibond/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:unibond/view/screens/user/join_screen.dart';
+import 'package:unibond/view/screens/user/modify_screen.dart';
 
 void main() {
   runApp(const MaterialApp(
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
         useMaterial3: true,
       ),
-      home: JoinScreen(),
+      home: const ModifyScreen(),
       // 임시 스플래시화면
     );
   }
