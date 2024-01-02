766585556ed6b4ceb94cde8d6ce60250cd720282
Nyeong <iwbm312@gmail.com>
2024-01-02 12:25:11 +0900

    Feat: 관심사 선택 수정 기능 구현
    - 관심사 4개 제한 필요함
    - 실제 post요청 수정 필요함

--- unibond/lib/view/screens/splash_screen.dart
+++ unibond/lib/view/screens/splash_screen.dart
@@ -1,52 +1,94 @@
-import 'package:flutter/material.dart';
-import 'package:unibond/resources/app_colors.dart';
-import 'package:unibond/view/screens/home_screen.dart';
+// import 'package:dio/dio.dart';
+// import 'package:flutter/material.dart';
+// import 'package:unibond/resources/app_colors.dart';
+// import 'package:unibond/util/auth_storage.dart';
+// import 'package:unibond/view/screens/home_screen.dart';
+// import 'package:unibond/view/screens/user/join_screen.dart';
+// import 'package:unibond/view/screens/user/root_tab.dart';
 
-class SplashScreen extends StatefulWidget {
-  const SplashScreen({super.key});
+// class SplashScreen extends StatefulWidget {
+//   const SplashScreen({super.key});
 
-  @override
-  State<SplashScreen> createState() => _SplashScreenState();
-}
+//   @override
+//   State<SplashScreen> createState() => _SplashScreenState();
+// }
 
-class _SplashScreenState extends State<SplashScreen> {
-  @override
-  void initState() {
-    super.initState();
-    _navigatetohome();
-  }
+// class _SplashScreenState extends State<SplashScreen> {
+//   @override
+//   void initState() {
+//     super.initState();
+//     _checkAuthAndNavigate();
+//   }
 
-  void _navigatetohome() async {
-    await Future.delayed(const Duration(milliseconds: 2000), () {});
-    // ignore: use_build_context_synchronously
-    Navigator.pushReplacement(
-        context, MaterialPageRoute(builder: (context) => const HomeScreen()));
-  }
+//   void _checkAuthAndNavigate() async {
+//     String? memberId = await AuthStorage.getAuthToken();
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          color: AppColors.contentColorWhite,
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Center(
-                  child: Image.asset(
-                    'assets/images/logo.png',
-                    width: 200,
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
+//     if (memberId != null) {
+//       // 인증된 사용자면
+//       try {
+//         final profile = await getMyProfile(memberId);
+//         if (profile.isSuccess) {
+//           Navigator.pushReplacement(context,
+//               MaterialPageRoute(builder: (context) => const RootTab()));
+//         } else {
+//           // 프로필 조회 실패
+//           _navigateToRegister();
+//         }
+//       } catch (e) {
+//         // 오류 처리
+//         _navigateToRegister();
+//       }
+//     } else {
+//       // 인증되지 않은 사용자
+//       _navigateToRegister();
+//     }
+//   }
+
+//   void _navigateToRegister() {
+//     Navigator.pushReplacement(
+//       context,
+//       MaterialPageRoute(
+//         builder: (context) => JoinScreen(),
+//       ),
+//     );
+//   }
+
+//   // 나의 프로필 조회 함수
+//   Future<UserProfile> getMyProfile(String memberId) async {
+//     final dio = Dio();
+//     final response = await dio.get(
+//       'http://3.35.110.214/api/v1/members/$memberId',
+//       options: Options(headers: {'Authorization': memberId}),
+//     );
+//     return UserProfile.fromJson(response.data);
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Container(
+//         decoration: const BoxDecoration(
+//           color: AppColors.contentColorWhite,
+//         ),
+//         child: Row(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             Column(
+//               mainAxisAlignment: MainAxisAlignment.center,
+//               children: [
+//                 Center(
+//                   child: Image.asset(
+//                     'assets/images/logo.png',
+//                     width: 200,
+//                   ),
+//                 ),
+//               ],
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+
+//   // Future<UserProfie> getMyProfile(String memberId) {}
+// }
