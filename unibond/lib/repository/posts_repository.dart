b9150ad06025a62bdb596fb78c8c3cde0eb9873e
Nyeong <iwbm312@gmail.com>
2024-01-07 02:41:58 +0900

    Feat: 게시물, 멤버 통신 기능 구현

--- unibond/lib/repository/posts_repository.dart
+++ unibond/lib/repository/posts_repository.dart
@@ -1,8 +1,12 @@
 import 'package:dio/dio.dart';
+import 'package:unibond/controller/dto/code_msg_res_dto.dart';
+import 'package:unibond/model/post/comment_post.dart';
+import 'package:unibond/model/post/exppost_prev.dart';
 import 'package:unibond/model/post/qnapost_request.dart';
+import 'package:unibond/util/userIdNum.dart';
 
 // 상세 게시물 조회
-Future<QnaPostDetail> getPostDetail(String postId, String memberId) async {
+Future<QnaPostDetail> getQnaPostDetail(String postId, String memberId) async {
   final dio = Dio();
   final response = await dio.get(
     'http://3.35.110.214/api/v1/community/$postId',
@@ -10,4 +14,29 @@ Future<QnaPostDetail> getPostDetail(String postId, String memberId) async {
   );
 
   return QnaPostDetail.fromJson(response.data);
+}
+
+/// 댓글 작성 POST, 29번 유저
+Future<CodeMsgDto> postComment(String postId, CommentPost commentPost) async {
+  final dio = Dio();
+  final response = await dio.post(
+    'http://3.35.110.214/api/v1/community/$postId/comments',
+    data: commentPost.toJson(),
+    options: Options(headers: {'Authorization': '29'}),
+  );
+  return CodeMsgDto.fromJson(response.data);
+}
+
+/// 경험 공유 미리보기 게시물 조회
+Future<ExpPostPrevRequest> getExpPostsList() async {
+  final dio = Dio();
+
+  dio.interceptors.add(LogInterceptor(
+    responseBody: true, // 응답 본문을 찍을지 여부
+  ));
+  final response = await dio.get(
+    'http://3.35.110.214/api/v1/community/experience',
+    options: Options(headers: {'Authorization': userIdNum ?? ""}),
+  );
+  return ExpPostPrevRequest.fromJson(response.data);
 }
