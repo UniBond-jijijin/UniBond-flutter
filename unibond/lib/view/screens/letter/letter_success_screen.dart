6db7f43a62e36f2847b5c09a804592c1a6fbde83
Nyeong <iwbm312@gmail.com>
2024-01-08 11:54:44 +0900

    Feat: 편지작성화면, 전송완료화면 UI, 라우트설정

--- unibond/lib/view/screens/letter/letter_success_screen.dart
+++ unibond/lib/view/screens/letter/letter_success_screen.dart
@@ -2,54 +2,60 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:unibond/domain/letter/letter.dart';
 import 'package:unibond/view/screens/letter/letter_box_screen.dart';
+import 'package:unibond/view/screens/user/root_tab.dart';
 import 'package:unibond/view/widgets/custom_elevated_button.dart';
 
-class LetterSuccessScreen extends StatefulWidget {
-  @override
-  _LetterSuccessScreenState createState() => _LetterSuccessScreenState();
-}
+class LetterSuccessScreen extends StatelessWidget {
+  const LetterSuccessScreen({super.key});
 
-class _LetterSuccessScreenState extends State<LetterSuccessScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         title: const Text('전송 완료'),
+        automaticallyImplyLeading: false,
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Positioned(
-              top: 0,
-              child: Image.asset(
-                'assets/images/send.png',
-                width: 100,
-                height: 100,
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.25,
+                    ),
+                    Image.asset(
+                      'assets/images/send.png',
+                      width: 100,
+                      height: 100,
+                    ),
+                    const SizedBox(height: 60),
+                    Image.asset(
+                      'assets/images/sendsuccess.png',
+                      width: 200,
+                    ),
+                    const SizedBox(height: 15),
+                    Image.asset(
+                      'assets/images/sendhour.png',
+                      width: 200,
+                    ),
+                    const SizedBox(height: 300),
+                  ],
+                ),
               ),
             ),
-            SizedBox(height: 60),
-
-            Image.asset(
-              'assets/images/sendsuccess.png',
-              width: 200,
-            ),
-            SizedBox(height: 15),
-
-            Image.asset(
-              'assets/images/sendhour.png',
-              width: 200,
-            ),
-            SizedBox(height: 300), // Add spacing between letters
-
-            // CustomElevatedButton at the bottom
-            CustomElevatedButton(
-              text: "편지함으로",
-              screenRoute: () {
-                Get.to(() => LetterBoxScreen());
-              },
+            Padding(
+              padding: const EdgeInsets.only(bottom: 16.0),
+              child: CustomElevatedButton(
+                text: "편지함으로",
+                screenRoute: () {
+                  Get.to(() => const RootTab(initialIndex: 1));
+                },
+              ),
             ),
           ],
         ),
--- unibond/lib/view/screens/letter/letter_write_screen.dart
+++ unibond/lib/view/screens/letter/letter_write_screen.dart
@@ -1,111 +1,106 @@
-// import 'package:flutter/material.dart';
-// import 'package:get/get.dart';
-// import 'package:unibond/controller/dto/letter_req_dto.dart';
-// import 'package:unibond/controller/letter_controller.dart';
-// import 'package:unibond/util/validator_util.dart';
-// import 'package:unibond/view/screens/home_screen.dart';
-// import 'package:unibond/view/screens/letter/letter_success_screen.dart';
-// import 'package:unibond/view/widgets/custom_elevated_button.dart';
-// import 'package:fluttertoast/fluttertoast.dart';
-// import 'package:get/get.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:unibond/model/letter/send_letter.dart';
+import 'package:unibond/repository/letters_repository.dart';
+import 'package:unibond/resources/toast.dart';
+import 'package:unibond/util/validator_util.dart';
+import 'package:unibond/view/screens/letter/letter_success_screen.dart';
+import 'package:unibond/view/widgets/custom_elevated_button.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
-// class LetterWriteScreen extends StatelessWidget {
-//   final _formKey = GlobalKey<FormState>();
+class LetterWriteScreen extends StatelessWidget {
+  final String receiverId;
+  final _formKey = GlobalKey<FormState>();
 
-//   final _titleController = TextEditingController();
-//   final _contentController = TextEditingController();
+  final _titleController = TextEditingController();
+  final _contentController = TextEditingController();
 
-//   LetterWriteScreen({Key? key}) : super(key: key);
+  LetterWriteScreen({Key? key, required this.receiverId}) : super(key: key);
 
-//   @override
-//   Widget build(BuildContext context) {
-//     final l = Get.find<LetterController>();
+  @override
+  Widget build(BuildContext context) {
+    // final l = Get.find<LetterController>();
 
-//     return Scaffold(
-//       appBar: AppBar(
-//         centerTitle: true,
-//         title: const Text('작성중인 편지'),
-//         leading: IconButton(
-//           icon: const Icon(Icons.arrow_back_ios),
-//           onPressed: () {
-//             Get.back();
-//           },
-//         ),
-//       ),
-//       body: Stack(
-//         children: [
-//           Container(
-//             decoration: BoxDecoration(
-//               image: DecorationImage(
-//                 image: AssetImage('assets/images/letterwritescreen.jpg'),
-//                 fit: BoxFit.cover,
-//               ),
-//             ),
-//           ),
-//           Padding(
-//             padding: const EdgeInsets.all(16.0),
-//             child: Form(
-//               key: _formKey,
-//               child: Column(
-//                 mainAxisAlignment: MainAxisAlignment.center,
-//                 children: [
-//                   TextField(
-//                     controller: _titleController,
-//                     decoration: const InputDecoration(
-//                       labelText: "제목",
-//                       border: OutlineInputBorder(),
-//                     ),
-//                   ),
-//                   const SizedBox(height: 16),
-//                   Expanded(
-//                     child: TextField(
-//                       controller: _contentController,
-//                       maxLines: null,
-//                       expands: true,
-//                       decoration: const InputDecoration(
-//                         hintText: "내용을 입력하세요...",
-//                         border: OutlineInputBorder(),
-//                       ),
-//                     ),
-//                   ),
-//                   SizedBox(height: 16),
-//                   CustomElevatedButton(
-//                     text: "전송하기", // Set the button color
-//                     screenRoute: () async {
-//                       if (isValid(_formKey)) {
-//                         var isSuccess = await l.sendLetter(
-//                           "30",
-//                           _titleController.text.trim(),
-//                           _contentController.text.trim(),
-//                         );
-//                         if (isSuccess == true) {
-//                           showToastMessage();
-//                           print('편지 전송 성공');
-//                           Get.off(() => LetterSuccessScreen());
-//                         } else {
-//                           print('편지 전송 실패');
-//                         }
-//                       }
-//                     },
-//                   ),
-//                 ],
-//               ),
-//             ),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text('편지 쓰기'),
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back_ios),
+            onPressed: () {
+              Get.back();
+            },
+          ),
+        ),
+        body: Stack(
+          children: [
+            Container(
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/letterwritescreen.jpg'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    TextField(
+                      controller: _titleController,
+                      decoration: const InputDecoration(
+                        labelText: "편지 제목을 입력해주세요.",
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                    Expanded(
+                      child: TextField(
+                        controller: _contentController,
+                        maxLines: null,
+                        expands: true,
+                        decoration: const InputDecoration(
+                          hintText: "오늘은 어떤 이야기를 하고 싶으신가요?",
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                    CustomElevatedButton(
+                      text: "전송하기", // Set the button color
+                      screenRoute: () async {
+                        if (isValid(_formKey)) {
+                          LetterPostRequest letterPostRequest =
+                              LetterPostRequest(
+                            receiverId: receiverId,
+                            title: _titleController.text.trim(),
+                            content: _contentController.text.trim(),
+                          );
 
-//   void showToastMessage() {
-//     Fluttertoast.showToast(
-//       msg: "전송이 완료되었습니다",
-//       toastLength: Toast.LENGTH_SHORT,
-//       gravity: ToastGravity.BOTTOM,
-//       timeInSecForIosWeb: 2,
-//       backgroundColor: Colors.grey,
-//       textColor: Colors.white,
-//       fontSize: 16.0,
-//     );
-//   }
-// }
+                          var codeMsgResDto =
+                              await postLetter(letterPostRequest);
+                          if (codeMsgResDto.isSuccess!) {
+                            Get.off(() => const LetterSuccessScreen());
+                          } else {
+                            showToastMessage(codeMsgResDto.msg!);
+                          }
+                        }
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
