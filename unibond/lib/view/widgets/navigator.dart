493748fc6bf7451404ccc8f9bead1adcc040f99b
codeJiwon <loopy0107@sookmyung.ac.kr>
2024-02-17 00:52:30 +0900

    Feat: 접근성 수정

--- unibond/lib/view/widgets/navigator.dart
+++ unibond/lib/view/widgets/navigator.dart
@@ -4,22 +4,43 @@ class MyBottomNavigationBar extends StatelessWidget {
   final int currentIndex;
   final Function(int) onTap;
 
-  const MyBottomNavigationBar(
-      {super.key, required this.currentIndex, required this.onTap});
+  const MyBottomNavigationBar({
+    Key? key,
+    required this.currentIndex,
+    required this.onTap,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(
-      currentIndex: currentIndex,
-      items: const [
-        BottomNavigationBarItem(
-            icon: Icon(Icons.home), label: '홈', tooltip: '홈으로 이동'),
-        BottomNavigationBarItem(
-            icon: Icon(Icons.mail), label: '편지함', tooltip: '편지함으로 이동'),
-        BottomNavigationBarItem(
-            icon: Icon(Icons.person), label: '프로필', tooltip: '프로필로 이동'),
-      ],
-      onTap: onTap,
+    return Semantics(
+      label: '하단 메뉴바',
+      child: BottomNavigationBar(
+        currentIndex: currentIndex,
+        items: [
+          BottomNavigationBarItem(
+            icon: Semantics(
+              label: '홈',
+              child: Icon(Icons.home),
+            ),
+            tooltip: '홈으로 이동',
+          ),
+          BottomNavigationBarItem(
+            icon: Semantics(
+              label: '편지함',
+              child: Icon(Icons.mail),
+            ),
+            tooltip: '편지함으로 이동',
+          ),
+          BottomNavigationBarItem(
+            icon: Semantics(
+              label: '프로필',
+              child: Icon(Icons.person),
+            ),
+            tooltip: '프로필로 이동',
+          ),
+        ],
+        onTap: onTap,
+      ),
     );
   }
 }
